e0b5284c7f320b0bc3d986a084e2ac2a1158fede
Phyth <Phyth@LAPTOP-T6QC85B5>
2024-10-20 23:51:11 -0400

    Finished tests

--- BCSRec.testing/BCSRec.testing.cpp
+++ BCSRec.testing/BCSRec.testing.cpp
@@ -1,5 +1,6 @@
 
 #include "CppUnitTest.h"
+#include <math.h>
 extern "C"{
 #include "../BCSRec/main.h"
 }
@@ -11,30 +12,107 @@ namespace BCSRectesting
 	{
 	public:
 		
-		TEST_METHOD(SetLength)
+		TEST_METHOD(GetArea)
+		{
+			int width = 2;
+			int length = 3;
+			double expected = length * width;
+
+			//ACT
+			double actual = getArea(&length, &width);
+
+			//ASSERT
+			Assert::AreEqual(expected, actual);
+		}
+		TEST_METHOD(getPerimeter)
+		{
+			int width = 2;
+			int length = 3;
+			double expected = length + length + width + width;
+
+			//ACT
+			double actual = getPerimeter(&length, &actual);
+
+			//ASSERT
+			Assert::AreEqual(expected, actual);
+		}
+		TEST_METHOD(SetLength1Standard)
 		{
 			int input = 2;
 			int length = 3;
 			int expected = 2;
 
 			//ACT
-			setLength(&input, &length);
+			int actual = 0;
+			setLength(input, &actual);
 
 			//ASSERT
-			Assert::AreEqual(expected, input);
-			Assert::AreEqual(expected, length);
+			Assert::AreEqual(expected, actual);
 		}
-		TEST_METHOD(GetArea)
+		TEST_METHOD(SetLength2Null)
 		{
-			int width = 2;
+			int input = 0;
 			int length = 3;
-			int expected = length * width;
+			int expected = 0;
 
 			//ACT
-			getArea(&length, &width);
+			int actual = 0;
+			setLength(input, &actual);
 
 			//ASSERT
-			Assert::AreEqual(expected, width * length);
+			Assert::AreEqual(expected, actual);
+		}
+		TEST_METHOD(SetLength3Negative)
+		{
+			int input = -2;
+			int length = 3;
+			int expected = -2;
+
+			//ACT
+			int actual = 0;
+			setLength(input, &actual);
+
+			//ASSERT
+			Assert::AreEqual(expected, actual);
+		}
+		TEST_METHOD(SetWidth1Standard)
+		{
+			int input = 2;
+			int width = 3;
+			int expected = 2;
+
+			//ACT
+			int actual = 0;
+			setWidth(input, &actual);
+
+			//ASSERT
+			Assert::AreEqual(expected, actual);
+		}
+		TEST_METHOD(SetWidth2Null)
+		{
+			int input = 0;
+			int width = 3;
+			int expected = 0;
+
+			//ACT
+			int actual = 0;
+			setWidth(input, &actual);
+
+			//ASSERT
+			Assert::AreEqual(expected, actual);
+		}
+		TEST_METHOD(SetWidth3Negative)
+		{
+			int input = -2;
+			int width = 3;
+			int expected = -2;
+
+			//ACT
+			int actual = 0;
+			setWidth(input, &actual);
+
+			//ASSERT
+			Assert::AreEqual(expected, actual);
 		}
 	};
 }
